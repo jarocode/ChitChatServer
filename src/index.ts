5e249191ea48cc5ed877ea8bba5d586cfcf7a3a2
jarocode <okolieuchenna890@gmail.com>
2022-10-10 14:56:53 +0100

    authentication middleware

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!')
+import express from "express";
+import { generateToken } from './api/utils/jwt.utils';
+
+const app = express();
+const port = 3000;
+
+// Only generate a token for lower level environments
+if (process.env.NODE_ENV !== 'production') {
+  console.log('JWT', generateToken());
+}
+
+// the rest logic
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
